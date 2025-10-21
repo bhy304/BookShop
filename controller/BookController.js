d5d9697779723ac79e661b3f268d0d9d6b41c982
Hayeon Baek <bhy0512@gmail.com>
2025-10-21 17:44:03 +0900

    refactor: Simplify allBooks function and remove BooksByCategory route

--- controller/BookController.js
+++ controller/BookController.js
@@ -3,16 +3,35 @@ const { StatusCodes } = require('http-status-codes')
 
 // 전체 도서 조회
 const allBooks = (req, res) => {
-  const sql = 'SELECT * FROM books'
+  const { category_id } = req.query
 
-  connection.query(sql, (err, results) => {
-    if (err) {
-      console.log(err)
-      return res.status(StatusCodes.BAD_REQUEST).end()
-    }
+  if (category_id) {
+    const sql = 'SELECT * FROM books WHERE category_id = ?'
 
-    return res.status(StatusCodes.OK).json(results)
-  })
+    connection.query(sql, category_id, (err, results) => {
+      if (err) {
+        console.log(err)
+        return res.status(StatusCodes.BAD_REQUEST).end()
+      }
+
+      if (results.length) {
+        return res.status(StatusCodes.OK).json(results)
+      } else {
+        return res.status(StatusCodes.NOT_FOUND).end()
+      }
+    })
+  } else {
+    const sql = 'SELECT * FROM books'
+
+    connection.query(sql, (err, results) => {
+      if (err) {
+        console.log(err)
+        return res.status(StatusCodes.BAD_REQUEST).end()
+      }
+
+      return res.status(StatusCodes.OK).json(results)
+    })
+  }
 }
 
 // 개별 도서 조회
@@ -35,23 +54,4 @@ const bookDetail = (req, res) => {
   })
 }
 
-// 카테고리별 도서 목록 조회
-const BooksByCategory = (req, res) => {
-  const { category_id } = req.query
-  const sql = 'SELECT * FROM books WHERE category_id = ?'
-
-  connection.query(sql, category_id, (err, results) => {
-    if (err) {
-      console.log(err)
-      return res.status(StatusCodes.BAD_REQUEST).end()
-    }
-
-    if (results.length) {
-      return res.status(StatusCodes.OK).json(results)
-    } else {
-      return res.status(StatusCodes.NOT_FOUND).end()
-    }
-  })
-}
-
-module.exports = { allBooks, bookDetail, BooksByCategory }
+module.exports = { allBooks, bookDetail }
--- routes/books.js
+++ routes/books.js
@@ -3,13 +3,11 @@ const router = express.Router()
 const {
   allBooks,
   bookDetail,
-  BooksByCategory,
 } = require('../controller/BookController')
 
 router.use(express.json())
 
 router.get('/', allBooks)
 router.get('/:id', bookDetail)
-router.get('/', BooksByCategory)
 
 module.exports = router
