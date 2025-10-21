93f819e0a918abbe7d35d6f5658b5e57c14fe0e1
Hayeon Baek <bhy0512@gmail.com>
2025-10-21 19:49:55 +0900

    feat: Add category route and controller for fetching all categories

---
+++ controller/CategoryController.js
@@ -0,0 +1,19 @@
+const connection = require('../mariadb')
+const { StatusCodes } = require('http-status-codes')
+
+const allCategory = (req, res) => {
+  const sql = 'SELECT * FROM category'
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.log(err)
+      return res.status(StatusCodes.BAD_REQUEST).end()
+    }
+
+    console.log(res)
+
+    return res.status(StatusCodes.OK).json(results)
+  })
+}
+
+module.exports = { allCategory }
