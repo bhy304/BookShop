8ba1992ff988dee978ebdff6aeb01f8201dfab02
Hayeon Baek <bhy0512@gmail.com>
2025-10-22 14:09:08 +0900

    fix: Correct SQL query construction in allBooks function to properly handle filtering and pagination

--- controller/BookController.js
+++ controller/BookController.js
@@ -6,19 +6,20 @@ const allBooks = (req, res) => {
   const { category_id, news, limit, currentPage } = req.query
 
   let offset = limit * (currentPage - 1)
-  let sql = 'SELECT * FROM books LIMIT ? OFFSET ?'
-  let values = [parseInt(limit), offset]
+  let sql = 'SELECT * FROM books'
+  let values = []
   if (category_id && news) {
     sql +=
       ' WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()'
-    values = values.push(category_id, news)
+    values.push(category_id)
   } else if (category_id) {
     sql += ' WHERE category_id = ?'
-    values = values.push(category_id)
+    values.push(category_id)
   } else if (news) {
     sql += ' WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()'
-    values = values.push(news)
   }
+  sql += ' LIMIT ? OFFSET ?'
+  values.push(parseInt(limit), offset)
 
   connection.query(sql, values, (err, results) => {
     if (err) {
