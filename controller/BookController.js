b61a405d54db7e5f5d06a77f4ca21ddcba7c5c4a
Hayeon Baek <bhy0512@gmail.com>
2025-10-22 13:03:55 +0900

    feat: Update allBooks function to support filtering by news and category, and add new book entries in data.sql

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,37 +1,37 @@
 const connection = require('../mariadb')
 const { StatusCodes } = require('http-status-codes')
 
-// 전체 도서 조회
+// (카테고리별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res) => {
-  const { category_id } = req.query
+  const { category_id, news, limit, currentPage } = req.query
 
-  if (category_id) {
-    const sql = 'SELECT * FROM books WHERE category_id = ?'
-
-    connection.query(sql, category_id, (err, results) => {
-      if (err) {
-        console.log(err)
-        return res.status(StatusCodes.BAD_REQUEST).end()
-      }
-
-      if (results.length) {
-        return res.status(StatusCodes.OK).json(results)
-      } else {
-        return res.status(StatusCodes.NOT_FOUND).end()
-      }
-    })
-  } else {
-    const sql = 'SELECT * FROM books'
-
-    connection.query(sql, (err, results) => {
-      if (err) {
-        console.log(err)
-        return res.status(StatusCodes.BAD_REQUEST).end()
-      }
-
-      return res.status(StatusCodes.OK).json(results)
-    })
+  let offset = limit * (currentPage - 1)
+  let sql = 'SELECT * FROM books LIMIT ? OFFSET ?'
+  let values = [parseInt(limit), offset]
+  if (category_id && news) {
+    sql +=
+      ' WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()'
+    values = values.push(category_id, news)
+  } else if (category_id) {
+    sql += ' WHERE category_id = ?'
+    values = values.push(category_id)
+  } else if (news) {
+    sql += ' WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()'
+    values = values.push(news)
   }
+
+  connection.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err)
+      return res.status(StatusCodes.BAD_REQUEST).end()
+    }
+
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results)
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end()
+    }
+  })
 }
 
 // 개별 도서 조회
