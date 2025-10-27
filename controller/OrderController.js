bfb6d21fae0fd324548bdc2a9f8ce83c78b57719
Hayeon Baek <bhy0512@gmail.com>
2025-10-27 14:22:50 +0900

    fix: Refactor order processing logic to correctly handle delivery and ordered books insertion

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -16,41 +16,38 @@ const order = (req, res) => {
 
   const { address, receiver, contact } = delivery
 
-  const orderedBooksSql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`
-  const deliveryId = 6
-  const orderId = 2
-
-  const values = []
-  items.forEach((item) => values.push([orderId, item.book_id, item.quantity]))
-
-  // connection.query(sql, [address, receiver, contact], (err, results) => {
-  connection.query(orderedBooksSql, [values], (err, results) => {
+  connection.query(sql, [address, receiver, contact], (err, results) => {
     if (err) {
       console.log(err)
       return res.status(StatusCodes.BAD_REQUEST).end()
     }
-    return res.status(StatusCodes.OK).json(results)
 
-    // const deliveryId = results.insertId
-    // const orderId = results.insertId
+    const orderId = results.insertId
+    const values = []
+    items.forEach((item) => values.push([orderId, item.book_id, item.quantity]))
 
-    // const orderSql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`
+    const orderedBooksSql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`
+    connection.query(orderedBooksSql, [values], (err, results) => {
+      if (err) {
+        console.log(err)
+        return res.status(StatusCodes.BAD_REQUEST).end()
+      }
 
-    // connection.query(
-    //   orderSql,
-    //   [firstBookTitle, totalQuantity, totalPrice, user_id, deliveryId],
-    //   (err, results) => {
-    //     if (err) {
-    //       console.log(err)
-    //       return res.status(StatusCodes.BAD_REQUEST).end()
-    //     }
+      const deliveryId = results.insertId
+      const orderSql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`
+      connection.query(
+        orderSql,
+        [firstBookTitle, totalQuantity, totalPrice, user_id, deliveryId],
+        (err, results) => {
+          if (err) {
+            console.log(err)
+            return res.status(StatusCodes.BAD_REQUEST).end()
+          }
 
-    //     const orderId = results.insertId
-    //     console.log(deliveryId, orderId)
-
-    //     return res.status(StatusCodes.OK).json(results)
-    //   }
-    // )
+          return res.status(StatusCodes.OK).json(results)
+        }
+      )
+    })
   })
 }
 
