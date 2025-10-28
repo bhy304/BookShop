f4932ee813679b0fd8c6187dba0e631f5713022e
Hayeon Baek <bhy0512@gmail.com>
2025-10-28 22:56:04 +0900

    refactor: Update OrderController and mariadb connection handling for improved async operations

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,54 +1,54 @@
-const connection = require('../mariadb')
+const getConnection = require('../mariadb')
+const mariadb = require('mysql2/promise')
 const { StatusCodes } = require('http-status-codes')
 
-const order = (req, res) => {
-  const {
-    items,
-    delivery,
-    totalQuantity,
-    totalPrice,
-    user_id,
-    firstBookTitle,
-  } = req.body
+const order = async (req, res) => {
+  const connection = await getConnection()
 
-  const sql =
-    'INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)'
+  try {
+    const {
+      items,
+      delivery,
+      totalQuantity,
+      totalPrice,
+      user_id,
+      firstBookTitle,
+    } = req.body
+    const { address, receiver, contact } = delivery
+    // delivery 테이블 삽입
+    const sql =
+      'INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)'
+    let [deliveries] = await connection.execute(sql, [
+      address,
+      receiver,
+      contact,
+    ])
+    const deliveryId = deliveries.insertId
 
-  const { address, receiver, contact } = delivery
+    // orders 테이블 삽입
+    const orderSql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`
+    let [orders] = await connection.execute(orderSql, [
+      firstBookTitle,
+      totalQuantity,
+      totalPrice,
+      user_id,
+      deliveryId,
+    ])
+    const orderId = orders.insertId
 
-  connection.query(sql, [address, receiver, contact], (err, results) => {
-    if (err) {
-      console.log(err)
-      return res.status(StatusCodes.BAD_REQUEST).end()
-    }
-
-    const orderId = results.insertId
+    // orderedBook 테이블 삽입
+    const orderedBooksSql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`
     const values = []
     items.forEach((item) => values.push([orderId, item.book_id, item.quantity]))
+    const [results] = await connection.query(orderedBooksSql, [values])
 
-    const orderedBooksSql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`
-    connection.query(orderedBooksSql, [values], (err, results) => {
-      if (err) {
-        console.log(err)
-        return res.status(StatusCodes.BAD_REQUEST).end()
-      }
-
-      const deliveryId = results.insertId
-      const orderSql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`
-      connection.query(
-        orderSql,
-        [firstBookTitle, totalQuantity, totalPrice, user_id, deliveryId],
-        (err, results) => {
-          if (err) {
-            console.log(err)
-            return res.status(StatusCodes.BAD_REQUEST).end()
-          }
-
-          return res.status(StatusCodes.OK).json(results)
-        }
-      )
-    })
-  })
+    return res.status(StatusCodes.OK).json(results)
+  } catch (error) {
+    console.log(error)
+    return res.status(StatusCodes.BAD_REQUEST).end()
+  } finally {
+    await connection.end()
+  }
 }
 
 const getOrders = (req, res) => {
--- mariadb.js
+++ mariadb.js
@@ -1,11 +1,13 @@
-const mariadb = require('mysql2')
+const mariadb = require('mysql2/promise')
 
-const connection = mariadb.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  dateStrings: true,
-})
+const getConnection = () => {
+  return mariadb.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dateStrings: true,
+  })
+}
 
-module.exports = connection
+module.exports = getConnection
