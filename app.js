35a95f537280dd5075811c9f64b93bfe881a0359
Hayeon Baek <bhy0512@gmail.com>
2025-11-26 12:19:02 +0900

    feat: enable and configure CORS middleware for specific origins and methods

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const dotenv = require('dotenv')
 dotenv.config()
+
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}
+
+app.use(cors(corsOptions))
 
 app.listen(process.env.PORT)
 
